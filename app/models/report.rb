e77c6e9c4d5baaa045ce1276365b67ee7aaf46e6
Juno Morey <j.morey@berkeley.edu>
2017-09-24 19:18:51 -0700

    add/edit reports javascript files

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,10 @@
 class Report < ActiveRecord::Base
+
+	validates :meetings_attended, presence: true
+  validates :current_projects, presence: true
+  validates :expenditures, presence: true
+  validates :other, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :user
 end
