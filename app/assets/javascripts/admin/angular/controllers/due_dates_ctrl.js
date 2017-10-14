36508b25e1eb072882316962b9be3e8232cdf002
LEON-MING <leonming12@yahoo.com>
2017-10-14 14:36:55 -0700

    added stuff

--- app/assets/javascripts/admin/angular/controllers/due_dates_ctrl.js
+++ app/assets/javascripts/admin/angular/controllers/due_dates_ctrl.js
@@ -6,10 +6,10 @@ angular.module('DueDatesCtrl', ['I18n', 'Flash', 'User'])
          * Allowed user roles.
          */
         var USER_ROLE_OPTIONS = [
-          { label: 'Admin', value: "admin"},
-          { label: 'Executive', value: "executive"},
-          { label: 'CAO', value: "cao"},
-          { label: 'Senator', value: "senator"}
+          { label: 'Admin', value: "1"},
+          { label: 'Executive', value: "2"},
+          { label: 'CAO', value: "3"},
+          { label: 'Senator', value: "4"}
         ];
 
         /**
@@ -22,6 +22,7 @@ angular.module('DueDatesCtrl', ['I18n', 'Flash', 'User'])
         };
 
         $scope.actionIndex = function () {
+            // Select a role to view its associated due dates
             $scope.queryBuilderOptions = {
               columns: [
                 {
@@ -34,11 +35,11 @@ angular.module('DueDatesCtrl', ['I18n', 'Flash', 'User'])
                 $scope.dataTableInstance.ajax.reload();
               }
             };
-            
+
             $scope.dataTableOptions = {
               serverSide: true,
               ajax: {
-                url: I18n.l('/admin/:locale/users.json'),
+                url: I18n.l('/admin/:locale/due_dates.json'),
                 // Just add the query builder filters to all AJAX requests sent by
                 // the data table!
                 data: function (d) {
@@ -50,43 +51,8 @@ angular.module('DueDatesCtrl', ['I18n', 'Flash', 'User'])
               columns: [
                 { data: 'id' },
                 { data: 'due_date' },
-                { data: 'role_id',
-                  searchable: false, orderable: false,
-                  render: function (data, type, row, meta) {
-                    return _.map(data, function (role) {
-                      return $filter('translate')(role);
-                    }).join(', ')
-                  }
-                },
-                // An example of bypassing the data table `row-ops` functionality,
-                // and instead manually setting up some row operations. Why we have
-                // done this: To show you its easily possible. Why you would do it:
-                // If the standard `row-ops` functionality doesn't support what you
-                // want to do.
-                //
-                // Also see the corresponding Angular view at
-                // /app/assets/javascripts/templates/admin/controllers/users/index.html,
-                // where a 'row ops' column has been manually added, to accommodate
-                // this column definition.
-                // { // data: 'actions', // Not really required for this column!
-                //   searchable: false, orderable: false,
-                //   className: 'dt-body-center',
-                //   render: function (data, type, row, meta) {
-                //     var editHtml =
-                //       '<a ui-sref="app.users.edit({ id: ' + row.id + ' })">'
-                //         + '<span class="glyphicon glyphicon-pencil"></span>' +
-                //       '</a>';
-                //
-                //     var deleteHtml =
-                //       '<a ng-click="deleteUser(' + row.id + ')">'
-                //         + '<span class="glyphicon glyphicon-remove"></span>' +
-                //       '</a>';
-                //
-                //     return editHtml + '&nbsp;' + deleteHtml;
-                //   }
-                // }
+                { data: 'role_id' }
               ],
-              order: [[2, 'asc']],
               stateSave: true, // Ensure table element has an id for this to work!
               // Save/load the query builder state along with the table state
               stateSaveParams: function (settings, data) {
@@ -101,44 +67,87 @@ angular.module('DueDatesCtrl', ['I18n', 'Flash', 'User'])
             // This is populated by the `datatable` directive.
             $scope.dataTableInstance = null;
 
+
+            // For operations on a single row
+            $scope.dataTableRowOps = {
+              edit: {
+                icon: 'glyphicon-pencil',
+                link: function (rowId) {
+                  return I18n.l('/:locale/posts/' + rowId + '/edit');
+                }
+              },
+              delete: {
+                icon: 'glyphicon-remove',
+                action: function (rowId) {
+                  I18n.confirm('Really delete post #' + rowId + '?',
+                    'really_delete_post_id', { id: rowId }
+                  ).then(function () {
+                    $scope.pleaseWaitSvc.request();
+                    // When performing an operation on a single row, unselect all
+                    // rows to avoid any ambiguity about the scope of the operation.
+                    $scope.dataTableSelectedRows.length = 0;
+
+                    Post.remove({ postId: rowId }, null,
+                      function (response) {
+                        $scope.pleaseWaitSvc.release();
+                        Flash.now.push('success', 'Post deleted.', 'post_deleted');
+
+                        $scope.dataTableInstance.ajax.reload();
+                      }, function (failureResponse) {
+                        $scope.pleaseWaitSvc.release();
+
+                        if (failureResponse.data.error) {
+                          // We assume messages from the server are localized, so we
+                          // don't need to provide a translation id.
+                          Flash.now.push('danger', failureResponse.data.error);
+                        } else {
+                          Flash.now.push('danger', 'Error deleting post.',
+                            'error_deleting_post');
+                        }
+                      });
+                  });
+                }
+              }
+            };
+
             // To enable row selection
             $scope.dataTableSelectedRows = [];
 
             // For bulk operations on currently selected rows
-            $scope.dataTableBulkOps = {}
-            //   deleteAll: {
-            //     name: 'Delete all',
-            //     action: function () {
-            //       I18n.confirm('Really delete users?',
-            //         'really_delete_users').then(function () {
-            //
-            //         $scope.pleaseWaitSvc.request();
-            //
-            //         User.batch_destroy({}, { ids: $scope.dataTableSelectedRows },
-            //           function (response) {
-            //             $scope.pleaseWaitSvc.release();
-            //             Flash.now.push('success', 'Users deleted.',
-            //               'users_deleted');
-            //
-            //             $scope.dataTableInstance.ajax.reload(); // Reload table data
-            //             $scope.dataTableSelectedRows.length = 0;
-            //           },
-            //           function (failureResponse) {
-            //             $scope.pleaseWaitSvc.release();
-            //
-            //             if (failureResponse.data.error) {
-            //               // We assume messages from the server are localized, so we
-            //               // don't need to provide a translation id.
-            //               Flash.now.push('danger', failureResponse.data.error);
-            //             } else {
-            //               Flash.now.push('danger', 'Error deleting users.',
-            //                 'error_deleting_users');
-            //             }
-            //           });
-            //       });
-            //     }
-            //   }
-            // };
+            $scope.dataTableBulkOps = {
+              deleteAll: {
+                name: 'Delete all',
+                action: function () {
+                  I18n.confirm('Really delete due dates?',
+                    'really_delete_users').then(function () {
+
+                    $scope.pleaseWaitSvc.request();
+
+                    User.batch_destroy({}, { ids: $scope.dataTableSelectedRows },
+                      function (response) {
+                        $scope.pleaseWaitSvc.release();
+                        Flash.now.push('success', 'Due dates deleted.',
+                          'users_deleted');
+
+                        $scope.dataTableInstance.ajax.reload(); // Reload table data
+                        $scope.dataTableSelectedRows.length = 0;
+                      },
+                      function (failureResponse) {
+                        $scope.pleaseWaitSvc.release();
+
+                        if (failureResponse.data.error) {
+                          // We assume messages from the server are localized, so we
+                          // don't need to provide a translation id.
+                          Flash.now.push('danger', failureResponse.data.error);
+                        } else {
+                          Flash.now.push('danger', 'Error deleting due dates.',
+                            'error_deleting_users');
+                        }
+                      });
+                  });
+                }
+              }
+            };
         }
 
     }]);
