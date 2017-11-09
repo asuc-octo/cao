015b9e8afc2b1f2a9fb23fcf95cdb6f0896b5c66
LEON-MING <leonming12@yahoo.com>
2017-11-09 04:11:38 -0800

    punctuality now shows up when you click users, color coded too

--- app/assets/javascripts/client/angular/controllers/punctuality_ctrl.js
+++ app/assets/javascripts/client/angular/controllers/punctuality_ctrl.js
@@ -17,9 +17,25 @@ angular.module('PunctualityCtrl', ['I18n', 'Flash', 'User'])
           };
           fetchUsers();
       };
-
-      $scope.getPunctuality = function(user) {
-          console.log(user.role);
+      $scope.formatDates = function(user) {
+          console.log("clicked!");
+          dateOptions = {
+              weekday: "long", year: "numeric", month: "long", day: "numeric"
+          };
+          for (i = 0; i < user.due_dates.length; i++) {
+              user.due_dates[i].deadline = (new Date(user.due_dates[i].deadline)).toLocaleDateString("en-us", dateOptions);
+          };
+        //   user.reports = user.due_dates;
+      };
+      $scope.getColor = function(report) {
+          punctuality = report.days_early;
+          if (punctuality == null) {
+              return "danger"
+          } else if (punctuality >= 0) {
+              return "success"
+          } else {
+              return "warning"
+          };
       };
 
     }]);
