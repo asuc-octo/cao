b1b86c44e4b8c4abdb1975dedcd8b5a61928407c
tempanyman <iju@berkeley.edu>
2017-11-12 15:58:37 -0800

    fixes database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102040134) do
+ActiveRecord::Schema.define(version: 20171019020016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,10 @@ ActiveRecord::Schema.define(version: 20171102040134) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "message"
+    t.string   "meetings_attended"
+    t.string   "current_projects"
+    t.string   "expenditures"
+    t.string   "other"
     t.integer  "user_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
@@ -138,7 +141,6 @@ ActiveRecord::Schema.define(version: 20171102040134) do
     t.datetime "updated_at"
     t.string   "authentication_token"
     t.string   "locale"
-    t.string   "name"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
