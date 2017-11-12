db11ceae7faea84fa867c2664cdb1deaad0c8bc6
tempanyman <iju@berkeley.edu>
2017-11-12 15:25:22 -0800

    fixes even more rebase conflicts

--- app/assets/javascripts/client/angular/controllers/home_ctrl.js
+++ app/assets/javascripts/client/angular/controllers/home_ctrl.js
@@ -2,6 +2,16 @@ angular.module('HomeCtrl', [])
   .controller('HomeCtrl', [
     '$scope',
     function($scope) {
+<<<<<<< HEAD
       $scope.hello = 'Welcome to the Office of the Chief Accountability Officer';
       $scope.submit = 'Submit a report';
     }]);
+=======
+      $scope.hello = 'testing this things!';
+      $scope.submit = 'Submit a report';
+      $scope.splash = [{
+        src:'assets/images/splash.jpg',
+      }];
+    }
+  ]);
+>>>>>>> f02d046... Finished homepage, with some extraneous files
