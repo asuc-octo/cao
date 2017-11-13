bedc623aee98829497a58e5c8ff77181a90741c6
LEON-MING <leonming12@yahoo.com>
2017-11-12 21:20:50 -0800

    tweaked the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,45 +22,39 @@ andrewchenk.add_role "senator"
 evancui.add_role "cao"
 
 # Create a bunch of due dates
+
+# We make a bunch of due dates to test different possibilities.
+# Here, Senators and CAO have the same due dates. However, one of the Senators
+# has done all the reports, and the other has done only some of them
+
 dueDate1 = DueDate.create! role_id: 2, deadline: DateTime.now + 1
 dueDate2 = DueDate.create! role_id: 2, deadline: DateTime.now + 2
 
-dueDate21 = DueDate.create! role_id: 3, deadline: DateTime.now - 15
-dueDate22 = DueDate.create! role_id: 3, deadline: DateTime.now - 14
-dueDate23 = DueDate.create! role_id: 3, deadline: DateTime.now - 13
-dueDate3 = DueDate.create! role_id: 3, deadline: DateTime.now - 2
-dueDate4 = DueDate.create! role_id: 3, deadline: DateTime.now - 1
-dueDate5 = DueDate.create! role_id: 3, deadline: DateTime.now
-dueDate51 = DueDate.create! role_id: 3, deadline: DateTime.now + 1
-dueDate52 = DueDate.create! role_id: 3, deadline: DateTime.now + 2
-
-dueDate53 = DueDate.create! role_id: 4, deadline: DateTime.now - 15
-dueDate54 = DueDate.create! role_id: 4, deadline: DateTime.now - 14
-dueDate55 = DueDate.create! role_id: 4, deadline: DateTime.now - 13
-dueDate6 = DueDate.create! role_id: 4, deadline: DateTime.now - 2
-dueDate7 = DueDate.create! role_id: 4, deadline: DateTime.now - 1
-dueDate8 = DueDate.create! role_id: 4, deadline: DateTime.now
-dueDate9 = DueDate.create! role_id: 4, deadline: DateTime.now + 1
-dueDate10 = DueDate.create! role_id: 4, deadline: DateTime.now + 2
+dueDate3 = DueDate.create! role_id: 3, deadline: DateTime.now - 15
+dueDate4 = DueDate.create! role_id: 3, deadline: DateTime.now - 14
+dueDate5 = DueDate.create! role_id: 3, deadline: DateTime.now - 13
+dueDate6 = DueDate.create! role_id: 3, deadline: DateTime.now - 2
+dueDate7 = DueDate.create! role_id: 3, deadline: DateTime.now - 1
+dueDate8 = DueDate.create! role_id: 3, deadline: DateTime.now
+dueDate9 = DueDate.create! role_id: 3, deadline: DateTime.now + 1
+dueDate10 = DueDate.create! role_id: 3, deadline: DateTime.now + 2
 
 
 # Create a bunch of reports
 Report.create! user_id: hahe.id, due_date_id: dueDate1.id, meetings_attended: 'what.'
 Report.create! user_id: hahe.id, due_date_id: dueDate2.id, meetings_attended: 'what2.'
+
 Report.create! user_id: arakeman.id, due_date_id: dueDate1.id, meetings_attended: 'what3.'
 
-Report.create! user_id: lionming.id, due_date_id: dueDate21.id, meetings_attended: 'hi.'
-Report.create! user_id: lionming.id, due_date_id: dueDate22.id, meetings_attended: 'hi2.'
-Report.create! user_id: lionming.id, due_date_id: dueDate23.id, meetings_attended: 'hi3.'
 Report.create! user_id: lionming.id, due_date_id: dueDate3.id, meetings_attended: 'hi.'
 Report.create! user_id: lionming.id, due_date_id: dueDate4.id, meetings_attended: 'hi2.'
 Report.create! user_id: lionming.id, due_date_id: dueDate5.id, meetings_attended: 'hi3.'
-Report.create! user_id: lionming.id, due_date_id: dueDate51.id, meetings_attended: 'hi.'
-Report.create! user_id: lionming.id, due_date_id: dueDate52.id, meetings_attended: 'hi2.'
+Report.create! user_id: lionming.id, due_date_id: dueDate6.id, meetings_attended: 'hi.'
+Report.create! user_id: lionming.id, due_date_id: dueDate7.id, meetings_attended: 'hi2.'
+Report.create! user_id: lionming.id, due_date_id: dueDate8.id, meetings_attended: 'hi3.'
+Report.create! user_id: lionming.id, due_date_id: dueDate9.id, meetings_attended: 'hi.'
+Report.create! user_id: lionming.id, due_date_id: dueDate10.id, meetings_attended: 'hi2.'
 
-Report.create! user_id: andrewchenk.id, due_date_id: dueDate3.id, meetings_attended: 'hi4.'
-Report.create! user_id: andrewchenk.id, due_date_id: dueDate4.id, meetings_attended: 'hi5.'
-
-
-# Create a post on behalf of the administrator
-admin.posts.create! message: 'Seeded post from the seeded administrator.'
+Report.create! user_id: andrewchenk.id, due_date_id: dueDate3.id, meetings_attended: 'hi.'
+Report.create! user_id: andrewchenk.id, due_date_id: dueDate4.id, meetings_attended: 'hi2.'
+Report.create! user_id: andrewchenk.id, due_date_id: dueDate5.id, meetings_attended: 'hi3.'
