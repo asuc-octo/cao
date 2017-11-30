bc3670913433e5ef91d8aff2d0ee6f314aa2ecb1
Andrew Chen <mochination@gmail.com>
2017-11-29 22:05:00 -0800

    Fixed a lot of stuff

--- app/controllers/api/users/registrations_controller.rb
+++ app/controllers/api/users/registrations_controller.rb
@@ -5,13 +5,14 @@ module Api
       respond_to :json
 
 # We don't want to create
-#      def create
-#        if AppSettings.get(:security, :force_sign_up_via_admin)
-#          render_op_error 'api.users', :sign_up_disabled
-#        else
-#          super
-#        end
-#      end
+      def create
+      #  AppSettings.set(:security, :force_sign_up_via_admin)
+        if AppSettings.get(:security, :force_sign_up_via_admin)
+          render_op_error 'api.users', :sign_up_disabled
+        else
+          super
+        end
+      end
 
       # update needs to be overridden
       # TODO Override the 'update' action
